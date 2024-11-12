cb543eee6cfab7aa7488ca02bd60601aa8943a4b
Salyin <113837319+Salyin@users.noreply.github.com>
2024-11-12 14:37:06 +0300

    base for calc

---
+++ code.cs
@@ -0,0 +1,73 @@
+using System;
+
+namespace Calculator
+{
+   class calc
+    {
+        int A;
+        int B;
+
+       int main()
+        {
+            bool stop = false;
+            string input;
+            while (stop = true)
+            {
+                input = console.ReadLine();
+                step2 (input);
+            }
+
+
+            string step2 (string input)
+            {
+
+                getAandB();
+                switch (input)
+                {
+                    case "+":
+                        getPlus ();
+                        break;
+
+                    case "-":
+                        getMinus () ;
+                        break;
+
+                    case "*":
+                        getUmn () ;
+                        break;
+                    
+                    case "/":
+                        getDel () ;
+                        break;
+
+
+                }
+            }
+
+             void getAandB()
+            {
+                
+            }
+
+            void getPlus()
+            {
+
+            }
+        
+            void getMinus()
+            {
+
+            }
+            void getUmn()
+            {
+
+            }
+            void getDel()
+            {
+
+            }
+        
+        }
+
+
+}
